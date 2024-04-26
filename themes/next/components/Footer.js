c37dd56c3a8e163f39219d4fb2d9ff4f7a27c25f
XU YICHENG <xu.yicheng@econ.ubbcluj.ro>
2024-04-26 13:42:45 +0300

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -19,33 +19,32 @@ const Footer = ({ title }) => {
   }, [])
 
   const formatTime = (totalSeconds) => {
-    const hours = Math.floor(totalSeconds / 3600)
+    const days = Math.floor(totalSeconds / 86400)
+    const hours = Math.floor((totalSeconds % 86400) / 3600)
     const minutes = Math.floor((totalSeconds % 3600) / 60)
     const seconds = Math.floor(totalSeconds % 60)
-    return `${hours}小时 ${minutes}分钟 ${seconds}秒`
+    return `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`
   }
 
   const copyrightDate = parseInt(since) < currentYear ? `${since}-${currentYear}` : `${currentYear}`
 
   return (
-        <footer
-            className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'
-        >
-            <span>
-                <DarkModeButton />
-                <i className='fas fa-clock' /> 运行时间: {formatTime(timeRunning)}<br />
-
-                <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={siteConfig('LINK')} className='underline font-bold'>{siteConfig('AUTHOR')}</a>.<br />
-
-                {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
-
-                <span className='hidden busuanzi_container_site_pv'>
-                    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
-                <span className='pl-2 hidden busuanzi_container_site_uv'>
-                    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-                <br />
-                <h1>{title}</h1>
+        <footer className='flex flex-col items-center justify-center w-full p-4 bg-white text-sm leading-6 dark:bg-gray-800 dark:text-gray-400'>
+            <DarkModeButton />
+            <span className='flex items-center justify-center'>
+                <i className='fas fa-clock'></i> 运行时间: {formatTime(timeRunning)}
             </span>
+            <span className='flex items-center justify-center animate-pulse'>
+                <i className='fas fa-heart'></i>
+            </span>
+            <a href={siteConfig('LINK')} className='underline font-bold'>{siteConfig('AUTHOR')}</a>
+            <br />
+            {siteConfig('BEI_AN') && (
+                <a href='https://beian.miit.gov.cn/' className='flex items-center justify-center'>
+                    <i className='fas fa-shield-alt'></i> {siteConfig('BEI_AN')}
+                </a>
+            )}
+            <h1 className='text-center'>{title}</h1>
         </footer>
   )
 }
