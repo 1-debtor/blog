8bfb8350d2db563c89571a62b4fdeb800e44f0e0
XU YICHENG <xu.yicheng@econ.ubbcluj.ro>
2024-04-26 17:54:30 +0300

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from 'react'
-import { siteConfig } from '@/lib/config'
-import DarkModeButton from '@/components/DarkModeButton'
 
-const Footer = ({ title }) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
-  const since = siteConfig('SINCE')
+const Footer = () => {
   const startDate = new Date('2024-01-14')
   const [timeRunning, setTimeRunning] = useState((Date.now() - startDate) / 1000) // Initial time in seconds
 
@@ -26,26 +21,12 @@ const Footer = ({ title }) => {
     return `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`
   }
 
-  const copyrightDate = parseInt(since) < currentYear ? `${since}-${currentYear}` : `${currentYear}`
-
   return (
-        <footer className='flex flex-col items-center justify-center w-full p-4 bg-white text-sm leading-6 dark:bg-gray-800 dark:text-gray-400'>
-            <DarkModeButton />
-            <span className='flex items-center justify-center'>
-                <i className='fas fa-clock'></i> 运行时间: {formatTime(timeRunning)}
-            </span>
-            <span className='flex items-center justify-center animate-pulse'>
-                <i className='fas fa-heart'></i>
-            </span>
-            <a href={siteConfig('LINK')} className='underline font-bold'>{siteConfig('AUTHOR')}</a>
-            <br />
-            {siteConfig('BEI_AN') && (
-                <a href='https://beian.miit.gov.cn/' className='flex items-center justify-center'>
-                    <i className='fas fa-shield-alt'></i> {siteConfig('BEI_AN')}
-                </a>
-            )}
-            <h1 className='text-center'>{title}</h1>
-        </footer>
+    <footer className='flex flex-col items-center justify-center w-full p-4 bg-white text-sm leading-6 dark:bg-gray-800 dark:text-gray-400'>
+      <span className='flex items-center justify-center'>
+        运行时间: {formatTime(timeRunning)} By DEBTOR SINCE@2024
+      </span>
+    </footer>
   )
 }
 
